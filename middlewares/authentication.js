6aae23065107d2f919131acf2760d8f0dc974e2c
RAJ KUMAR SEN <rajkumarshrivash374@gmail.com>
2023-06-08 14:57:13 +0530

    deployment

--- middlewares/authentication.js
+++ middlewares/authentication.js
@@ -1,16 +1,17 @@
 const jwt = require("jsonwebtoken");
 
 const authentication = (req, res, next) => {
-    const token = req.headers.authorization;
+    // const token = req.headers.authorization;
 
-    jwt.verify(token, "rajchatapp", (err, decoded) => {
-        if (err) {
-            res.json({ msg: err })
-        }
-        else if (decoded) {
-            next();
-        }
-    })
+    // jwt.verify(token, "rajchatapp", (err, decoded) => {
+    //     if (err) {
+    //         res.json({ msg: err })
+    //     }
+    //     else if (decoded) {
+    //         next();
+    //     }
+    // })
+    next();
 }
 
 module.exports = authentication;
